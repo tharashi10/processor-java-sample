8402d6e1f98d25181666712552c0ab760f325e91
tharashi10 <takahiro.harashima@oracle.com>
2022-04-15 12:47:19 +0900

    Initial Commit

---
+++ src/main/java/com/orcl/thara/poc/processor/sample/parse/JsonProcessorFunction.java
@@ -0,0 +1,7 @@
+package com.orcl.thara.poc.processor.sample.parse;
+
+public class JsonProcessorFunction {
+	public static void main(String[] args) {
+        System.out.println("Sample Exec.");
+    }
+}
---
+++ src/main/java/com/orcl/thara/poc/processor/sample/parse/infrastructure/OciJsonMapper.java
@@ -0,0 +1,5 @@
+package com.orcl.thara.poc.processor.sample.parse.infrastructure;
+
+public class OciJsonMapper {
+
+}
